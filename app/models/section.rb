3c49d0a2e7f8d116f5055fc2d1cc7ab481d8e6bf
Zach Herman <Zwherman@yahoo.com>
2019-10-28 11:44:37 -0500

    Video error/cleaning up other errors

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,9 +2,4 @@ class Section < ApplicationRecord
   belongs_to :course
   has_many :lessons
 
-  def next_section
-    section = course.sections.where("row_order > ?", self.row_order).rank(:row_order).first
-    return section
-  end
-
 end
